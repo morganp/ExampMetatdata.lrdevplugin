00adaf2b8ac0516674062d944f28c17605b77466
Morgan Prior <morgan@amaras-tech.co.uk>
2014-04-09 00:46:45 +0100

    Tag set example loads, views correctly

---
+++ Info.lua
@@ -0,0 +1,8 @@
+return {
+  LrSdkVersion = 5.0,
+  LrSdkMinimumVersion = 5.0,
+  LrToolkitIdentifier = 'sample.metadata',
+  LrPluginName = LOC "$$$/CustomMetadata/PluginName=Metadata Sample",
+  --LrMetadataProvider = 'SampleMetadataDefinition.lua', 
+  LrMetadataTagsetFactory = 'SampleTagset.lua',
+}
---
+++ SampleTagset.lua
@@ -0,0 +1,28 @@
+return {
+  title = LOC "$$$/SampleTagset/Title=Sample Tagset from Plug-in",
+  id = 'sampleTagset',
+  items = {
+    'com.adobe.copyright', 
+    'com.adobe.rightsUsageTerms', 
+    'com.adobe.copyrightInfoURL',
+    'com.adobe.separator',
+    'com.adobe.copyrightState',
+    { 
+      'com.adobe.caption', height_in_lines = 3
+    },
+    'com.adobe.separator',
+    { 
+      'com.adobe.label', 
+      label = LOC "$$$/Metadata/SampleLabel=Section Label"
+    },
+    'com.adobe.dateCreated',
+    'com.adobe.location',
+    'com.adobe.city',
+    'com.adobe.state',
+    'com.adobe.country',
+    'com.adobe.isoCountryCode',
+    'com.adobe.GPS',
+    'com.adobe.GPSAltitude',
+    'com.adobe.lightroom.metadata.sample.randomString',
+  },
+}
